76128fa7561526f851ae3e56ef4548a9a232f4c3
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2023-09-03 01:48:09 +0530

    Introduced parcel

--- app.js
+++ app.js
@@ -6,20 +6,24 @@
 </div>
 */
 
+import React from "react"
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
 	"h1",
 	{ id: "heading", key: "headkey" },
 	"Starting the react web-series journey."
 );
 
-const para = React.createElement("p", {class: "text", key:"paraKey"}, "Let's go.")
+const para = React.createElement(
+	"p",
+	{ class: "text", key: "paraKey" },
+	"Let's go."
+);
 
 const child = React.createElement("div", { id: "child" }, [heading, para]);
 
 const parent = React.createElement("div", { id: "parent" }, child);
-
-
-
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
