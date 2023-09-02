642c3d0ffa974628e17e904ba6f42d033444d725
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2023-09-02 17:23:33 +0530

    Added nested elements

--- inception-1/app.js
+++ inception-1/app.js
@@ -1,9 +1,27 @@
+/*
+<div id="parent">
+    <div id="child">
+        <h1></h1>
+    </div>
+</div>
+*/
+
 const heading = React.createElement(
 	"h1",
-	{id: "heading"},
+	{ id: "heading", key: "headkey" },
 	"Starting the react web-series journey."
 );
+
+const para = React.createElement("p", {class: "text", key:"paraKey"}, "Let's go.")
+
+const child = React.createElement("div", { id: "child" }, [heading, para]);
+
+const parent = React.createElement("div", { id: "parent" }, child);
+
+
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 
-console.log(heading)
+console.log(parent);
