c45a6e61dca135fd9d595ca6adba9b86b5af3d40
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2023-09-08 22:18:53 +0530

    JSX vs React Core

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
-import React from "react"
+import React from "react";
 import ReactDOM from "react-dom/client";
 
+const jsxElement = (
+	<h1>here we go with the chapter one of react and three of the course.</h1>
+);
+
+const heading = React.createElement("h1", { id: "heading" }, "Chapter: 3-1");
+
+console.log(jsxElement);
+console.log(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxElement);
