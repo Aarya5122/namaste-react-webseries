dbeecab1b0b8879fa70edd6163d734cc40b7025b
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2023-09-08 23:06:02 +0530

    React function component

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxElement = (
+const JsxElement = () => (
 	<h1>here we go with the chapter one of react and three of the course.</h1>
 );
 
-const heading = React.createElement("h1", { id: "heading" }, "Chapter: 3-1");
-
-console.log(jsxElement);
-console.log(heading);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(jsxElement);
+root.render(JsxElement());
