fc62a4ee262ffcc7d9f682e468113dc452fa3b9d
Aarya Nanndaann Singh M N <aarya.nanndaann.singh.m.n@gmail.com>
2023-09-08 21:52:23 +0530

    Refershed on creation of HTML elements.

--- app.js
+++ app.js
@@ -1,32 +1,3 @@
-/*
-<div id="parent">
-    <div id="child">
-        <h1></h1>
-    </div>
-</div>
-*/
-
 import React from "react"
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-	"h1",
-	{ id: "heading", key: "headkey" },
-	"Starting the react web-series journey."
-);
-
-const para = React.createElement(
-	"p",
-	{ class: "text", key: "paraKey" },
-	"Let's go."
-);
-
-const child = React.createElement("div", { id: "child" }, [heading, para]);
-
-const parent = React.createElement("div", { id: "parent" }, child);
-
-const roots = ReactDOM.createRoot(root);
-roots.render(heading);
-console.log(root)
-
-console.log(parent);
